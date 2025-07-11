704eb3446f52caf24eb3c53b6995dfd247e75b49
Zumar <zumar.zehgeer@iquasar.com>
2025-07-11 18:00:22 +0530

    adding healthcheck method to App

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,26 @@
 package app
 
-import ( 
+import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 )
 
 type Application struct {
-	Logger *log.Logger 
+	Logger *log.Logger
 }
 
-func NewApplication (*Application, error) {
+func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	app := &Application{
-		Logger: Logger,
+		Logger: logger,
 	}
 
 	return app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "status is available\n")
 }
--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"fem_project/internal/app"	
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"fem_project/internal/app"
 )
 
-func main(){
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
 	app, err := app.NewApplication()
-	if (err != nil) {
+	if err != nil {
 		panic(err)
 	}
 
-	app.Logger.Println("we are running our app")
+	http.HandleFunc("/health", app.HealthCheck)
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	app.Logger.Printf("we are running on port %d\n", port)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
 }
